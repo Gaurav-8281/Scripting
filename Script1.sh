cc78589b16f1352a16ec56dbc986967299d24efa
Gaurav-8281 <gauravpatil70450@gmail.com>
2025-01-09 09:22:33 +0530

    This is my commit from b1

--- Script1.sh
+++ Script1.sh
@@ -2,4 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo " Am Gaurav from DevOps"
-echo "Am learnig DevOps"
+echo "Am learnig DevOps"git 
+echo "Am adding this line from branch b1"
